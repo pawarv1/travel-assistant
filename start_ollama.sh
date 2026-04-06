c781f493545ae3c1c8e6f6293a3c6cca9cf17189
AidanS39 <aidan.stoner39@gmail.com>
2026-04-06 00:58:55 -0400

    added agent file, haven't connected to backend yet

---
+++ start_ollama.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#SBATCH --chdir=./                                # Set the working directory
+#SBATCH --mail-user=stonera3@tcnj.edu             # Who to send emails to
+#SBATCH --mail-type=ALL                           # Send emails on start, end and failure
+#SBATCH --job-name=ollama_server                  # Name to show in the job queue
+#SBATCH --output=./output/job.%j.out              # Name of stdout output file (%j expands to jobId)
+#SBATCH --ntasks=16                               # Total number of mpi tasks requested (CPU Cores basically)
+#SBATCH --nodes=1                                 # Total number of nodes requested
+#SBATCH --partition=gpu		                        # Partition (a.k.a. queue) to use
+#SBATCH --gres=gpu:1		                          # Select GPU resource (# after : indicates how many)
+#SBATCH --constraint=[l40s]    # use rtxa5000, l40s or gtx1080ti here to limit GPU selection
+
+module add ollama
+export OLLAMA_MODELS=/scratch/$USER/ollama_models
+ollama serve
